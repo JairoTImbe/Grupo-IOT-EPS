665db0b66e0e0242500abd56db2c056ac21cbb17
JairoTImbe <80768045+JairoTImbe@users.noreply.github.com>
2021-03-28 11:46:07 -0500

    prog a datos propios
    se modifica el codigo con los parametros propios de la placa y cuenta de ubidots usada por jairo. con la cual se realizara la primera prueba

--- Programacion_NodeMcu/Programacion_NodeMcu.ino
+++ Programacion_NodeMcu/Programacion_NodeMcu.ino
@@ -7,13 +7,13 @@
 
 #include "Ubidots.h"
 
-#define DEVICE  "dc4f2218xxx"  // Put here your Ubidots device label
+#define DEVICE  "esp8266_principal"  // Put here your Ubidots device label
 #define VARIABLE  "var_led"  // Put here your Ubidots variable label
-#define ID_TEMP "var_temperatura" 
-#define TOKEN  "xxxxxx"  // Put here your Ubidots TOKEN
+#define ID_TEMP "6060a75f1d84720a0164dab8" 
+#define TOKEN  "LOdit4otMYbe9Squ90k6C2mbmRUSeX"  // Put here your Ubidots TOKEN
 
-#define WIFISSID "SSID TU ROUTER" // Put here your Wi-Fi SSID
-#define PASSWORD "TU CLAVE" // Put here your Wi-Fi password
+#define WIFISSID "CanarNet_Familia Timbe" // Put here your Wi-Fi SSID
+#define PASSWORD "bj_A8DePraty" // Put here your Wi-Fi password
 
 Ubidots client(TOKEN);
 float Valor_Temperatura = 0;
