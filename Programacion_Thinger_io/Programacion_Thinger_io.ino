bdd62ab51dfd94255272955565f3e7388c8daaac
JairoTImbe <80768045+JairoTImbe@users.noreply.github.com>
2021-04-02 15:08:49 -0500

    configuracion thinger.io
    configuracion exitosa

--- Programacion_Thinger_io/Programacion_Thinger_io.ino
+++ Programacion_Thinger_io/Programacion_Thinger_io.ino
@@ -1,5 +1,7 @@
+#define _DISABLE_TLS_
+// other includes goes here
 #include <ThingerESP8266.h>
-
+//configuraciones para la plataforma
 #define USERNAME "JairoTimbe"
 #define DEVICE_ID "abc"
 #define DEVICE_CREDENTIAL "kcGTbFTMI28kI&"
@@ -9,18 +11,34 @@
 
 ThingerESP8266 thing(USERNAME, DEVICE_ID, DEVICE_CREDENTIAL);
 
-void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
+//variables globales
+float celsius=0;
+int LED_D1=5;
+int SensorTemp=A0;
 
+void setup() {
+  Serial.begin(115200); 
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED_D1, OUTPUT);
+  //conf wifi
   thing.add_wifi(SSID, SSID_PASSWORD);
 
   // digital pin control example (i.e. turning on/off a light, a relay, configuring a parameter, etc)
   thing["led"] << digitalPin(LED_BUILTIN);
-
+  thing["LED_D1"] << digitalPin(LED_D1);
   // resource output example (i.e. reading a sensor value)
-  thing["millis"] >> outputValue(millis());
+  thing["millis"] >> outputValue(celsius);
 }
 
 void loop() {
+
   thing.handle();
+  //lectura del sensor de temperatura
+  int analogValue = analogRead(SensorTemp);
+  float millivolts = (analogValue/1024.0) * 3300; //3300 is the voltage provided by NodeMCU
+  celsius = millivolts/10;
+  Serial.println(celsius);
+  delay(500);
+  
+  
 }
